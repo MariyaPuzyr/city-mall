f229469892524b5e4cc39d7629b03332fd25a4ea
AyriRin <mariyapyzur@gmail.com>
2018-11-30 16:55:20 +0200

    Правки

--- gulpfile.js
+++ gulpfile.js
@@ -1,82 +1,131 @@
-var gulp = require('gulp'), // подключаем Gulp
-    sass = require('gulp-sass'), // подключаем Gulp SASS
-    concat = require('gulp-concat'),
-    watch = require('gulp-watch'),
-    rename = require('gulp-rename'),
-    obfuscator = require('gulp-js-obfuscator'),
-    uglify = require('gulp-uglify'),
-    browserSync = require('browser-sync'),
-    del = require('del'),
-    pug = require('gulp-pug'),
-    fileinclude = require('gulp-file-include'),
-    gulpRemoveHtml = require('gulp-remove-html');
+var gulp           = require('gulp'),
+    sass           = require('gulp-sass'),
+    browserSync    = require('browser-sync'),
+    concat         = require('gulp-concat'),
+    uglify         = require('gulp-uglify'),
+    cleanCSS       = require('gulp-clean-css'),
+    rename         = require('gulp-rename'),
+    del            = require('del'),
+    imagemin       = require('gulp-imagemin'),
+    pngquant       = require('imagemin-pngquant'),
+    cache          = require('gulp-cache'),
+    autoprefixer   = require('gulp-autoprefixer'),
+    notify         = require('gulp-notify'),
+    fileinclude    = require('gulp-file-include'),
+    babel          = require('gulp-babel'),
+    gulpRemoveHtml = require('gulp-remove-html'),
+    concat         = require('gulp-concat'),
+    pug            = require('gulp-pug');
 
-var tasks = [];
 
-(function (mix) {
-    /* It works. Uncomment when it will be need */
-    mix.copy(['frontend/public/fonts/**/*'], './public/fonts');  // mix.copy(source_path, store_path)
-    mix.copy(['frontend/public/pic/**/*'], './public/pic');
-    mix.copy(['frontend/public/css/*.css'], './public/css');
-    mix.copy(['frontend/public/js/jquery.imagemapster.js'], './public/js');
-
-    // mix.gensass('frontend/public/css/*.scss', 'public/css');
-
-})(getTaskRunner());
-
-function getTaskRunner() {
-    return {
-        copy: function (followPath, copyPath) {
-            var taskName = this.changeTaskNameIfExists('files:copy');
-            gulp.task(taskName, function () {
-                gulp.src(followPath)
-                    .pipe(gulp.dest(copyPath))
-                    .on('end', function () {
-                        console.log('Files copied succesfully! Task: ' + taskName);
-                    });
-            });
-            tasks.push(taskName);
+gulp.task('browser-sync', function () {
+    browserSync({
+        server: {
+            baseDir: 'public/test'
         },
-        watch: function () {
-            gulp.task('watch', function () {
-                gulp.watch('frontend/css/**/*.css', ['commonStyle:build', 'commonStyle:build_1']);
-                gulp.watch('frontend/js/**/*.js', ['scripts:build', 'commonScript:build', 'commonScript:build_1']);
-                console.log('Start watching...');
-            });
-        }(),
-        // gensass: function (followPath, copyPath) {
-        //     gulp.task('sass', function() {
-        //         return gulp.src(followPath)
-        //             .pipe(sass())
-        //             .pipe(concat('styles.css'))
-        //             .pipe(gulp.dest(copyPath))
-        //
-        //     });
-        // },
-        clean: function () {
-            gulp.task('clean', function () {
-                return del(['./public/js/**/*', './public/css/**/*', './public/pic/**/*']);
-            });
-        }(),
-        changeTaskNameIfExists: function (taskName, freeIdx = 0) {
-            var idx = tasks.indexOf(taskName);
-            if (idx != -1) {
-                ++freeIdx;
-                taskName = this.changeTaskNameIfExists(taskName + '_' + freeIdx, freeIdx);
-            }
-            return taskName;
-        }
-    }
-}
+        notify: false
+    });
+});
 
 /* Gulp Sass */
 gulp.task('sass', function () {
     return gulp.src('frontend/public/css/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('frontend/public/css'))
+        .pipe(sass().on(('error'), notify.onError()))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(autoprefixer(['last 15 versions']))
+        .pipe(cleanCSS())
         .pipe(gulp.dest('public/css'))
         .pipe(browserSync.reload({stream: true}))
 });
+
+/* build libs styles*/
+gulp.task('styles', function () {
+    return gulp.src([
+            'public/libs/bootstrap/dist/css/bootstrap.css',
+            'public/libs/slick-carousel/slick/slick.css',
+            'public/libs/slick-carousel/slick/slick-theme.css'
+        ])
+        .pipe(concat("libs.min.css"))
+        .pipe(gulp.dest('public/css'))
+});
+
+gulp.task('js', function () {
+    return gulp.src([
+            'frontend/public/js/**/*',
+            '!frontend/public/js/jquery.imagemapster.js'
+        ])
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('public/js'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('buildhtml', function () {
+    gulp.src([
+        'frontend/public/templates/pages/!*.pug',
+        '!frontend/public/templates/components/!*.pug',
+        '!frontend/public/templates/layout.pug'
+    ])
+        .pipe(pug({}))
+        .pipe(fileinclude({
+            prefix: '@@'
+        }))
+        .pipe(gulpRemoveHtml())
+        .pipe(gulp.dest('public'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('libs', function () {
+    return gulp.src([
+        'frontend/public/libs/jquery/dist/jquery.js',
+        'frontend/public/libs/bootstrap/dist/js/bootstrap.js',
+        'frontend/public/libs/slick-carousel/slick/slick.js',
+        'frontend/public/js/jquery.imagemapster.js'
+    ])
+        .pipe(concat('libs.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('public/js'));
+});
+
+gulp.task('watch', ['buildhtml', 'sass', 'js', 'libs', 'imagemin', 'styles'], function () {
+    gulp.watch('frontend/public/css/**/*.scss', ['sass']);
+    gulp.watch([
+        'frontend/public/templates/pages/*.pug',
+        'frontend/public/templates/layout.pug',
+        'frontend/public/templates/components/*.pug'
+    ], ['buildhtml']);
+    gulp.watch('frontend/public/js/**/*.js', ['js']);
+    gulp.watch('frontend/public/images/**/*', ['imagemin']);
+});
+
+gulp.task('imagemin', function () {
+    return gulp.src('frontend/public/images/**/*')
+        .pipe(cache(imagemin({
+            interlaced: true,
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        })))
+        .pipe(gulp.dest('public/images'));
+});
+
+gulp.task('build', ['buildhtml', 'imagemin', 'sass', 'js', 'libs', 'browser-sync', 'styles'], function () {
+    var buildCss = gulp.src([
+        'frontend/public/css/styles.min.css'
+    ]).pipe(gulp.dest('public/css'));
+
+    var buildFonts = gulp.src('public/fonts/**/*')
+        .pipe(gulp.dest('public/fonts'));
+});
+
+gulp.task('cleancache', function () {
+    return cache.clearAll();
+});
+
+gulp.task('default', ['watch']);
+
+/*
 
 gulp.task('scripts', function () {
     return gulp.src([
@@ -94,11 +143,11 @@ gulp.task('scripts', function () {
         .pipe(gulp.dest('public/js'));
 });
 
-/* pug */
+/!* pug *!/
 gulp.task('buildhtml', function () {
     gulp.src([
-        'frontend/public/templates/pages/*.pug',
-        '!frontend/public/templates/components/*.pug',
+        'frontend/public/templates/pages/!*.pug',
+        '!frontend/public/templates/components/!*.pug',
         '!frontend/public/templates/layout.pug'
     ])
         .pipe(pug({}))
@@ -110,13 +159,12 @@ gulp.task('buildhtml', function () {
         .pipe(browserSync.reload({stream: true}));
 });
 gulp.task('default', ['clean', 'sass', 'scripts', 'buildhtml'], function () {
-    gulp.start(tasks);
     gulp.watch([
-        'frontend/public/templates/pages/*.pug',
+        'frontend/public/templates/pages/!*.pug',
         'frontend/public/templates/layout.pug',
-        'frontend/public/templates/components/*.pug'
+        'frontend/public/templates/components/!*.pug'
     ], ['buildhtml']);
-    gulp.watch('frontend/public/css/*.scss', ['sass']);
-    gulp.watch('frontend/public/js/*.js', ['scripts']);
+    gulp.watch('frontend/public/css/!*.scss', ['sass']);
+    gulp.watch('frontend/public/js/!*.js', ['scripts']);
     console.log('Please waiting...');
-});
+});*/
